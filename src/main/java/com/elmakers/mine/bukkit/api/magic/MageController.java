d706110b19455cc50d6510ba02a66e7ff929fec1
Nathan Wolf <nathan@elmakers.com>
2016-03-09 19:57:15 -0800

    Add basic item registry API (cherry picked from commit 950a5b8)

--- src/main/java/com/elmakers/mine/bukkit/api/magic/MageController.java
+++ src/main/java/com/elmakers/mine/bukkit/api/magic/MageController.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 import com.elmakers.mine.bukkit.api.block.CurrencyItem;
 import com.elmakers.mine.bukkit.api.block.MaterialAndData;
 import com.elmakers.mine.bukkit.api.entity.EntityData;
+import com.elmakers.mine.bukkit.api.item.MagicItem;
 import com.elmakers.mine.bukkit.api.maps.MapController;
 import com.elmakers.mine.bukkit.api.wand.WandUpgradePath;
 import org.bukkit.Location;
@@ -317,4 +318,8 @@ public interface MageController {
     public String getBlockSkin(Material blockType);
     public String getMobSkin(EntityType mobType);
     public boolean commitOnQuit();
+    
+    public Set<String> getItemKeys();
+    public MagicItem getItem(String key);
+    public MagicItem getItem(ItemStack match);
 }
